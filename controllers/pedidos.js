86ac3be0f5ee29e22b73517c0d89027722363136
CristianChipana <cristianchipanahuaman@gmail.com>
2022-06-28 22:46:04 -0500

    main

--- controllers/pedidos.js
+++ controllers/pedidos.js
@@ -67,7 +67,16 @@ const getPedidos = async (req = request, res = response) => {
                 msg: 'No se encontro pedidos del usuario'
             });
         }
+
+        // const [total, productos] = await Promise.all([
+        //     Producto.countDocuments({ estado: true }),
+    
+        //     Producto.find({ estado: true }).populate("usuario", ["_id"])
+        //         .populate('categoria', ["_id", "usuario", "nombre"])
+        // ]);
+
         const pedidos = await Pedido.find({ estado: true, bodega: existePedidos._id });
+
         res.status(200).json({
             success: true,
             data: pedidos
@@ -88,7 +97,9 @@ const getPedidosByIdUser = async (req = request, res = response) => {
 
         const { _id } = req.usuario
 
-        const pedidos = await Pedido.find({ estado: true, usuario : _id });
+        const pedidos = await Pedido.find({ estado: true, usuario : _id })
+        .populate("bodega");
+        
         res.status(200).json({
             success: true,
             data: pedidos
